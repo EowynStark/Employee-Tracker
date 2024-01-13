e386c219257556002548fed10d2132ea37d811e1
EowynStark <andwellm89@gmail.com>
2024-01-13 08:58:03 -0600

    adding role table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,11 @@ CREATE TABLE department (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
