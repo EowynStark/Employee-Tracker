d02db0bb90f5590b7c83a1e1fd764b854693ffed
EowynStark <andwellm89@gmail.com>
2024-01-13 10:06:26 -0600

    adding values for roles table in seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,10 @@ INSERT INTO departments (name)
 VALUES  ('Marketing'),
         ('IT'),
         ('Sales');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ('Marketing Specialist', 60000, 1),
+        ('Software Developer', 80000, 2),
+        ('Sales Representative', 70000, 3);
+
+
