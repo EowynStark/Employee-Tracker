d3959638afea833cb7fdb727202b61a201a4b43a
EowynStark <andwellm89@gmail.com>
2024-01-13 10:16:47 -0600

    adding values for employees table in seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,4 +8,7 @@ VALUES  ('Marketing Specialist', 60000, 1),
         ('Software Developer', 80000, 2),
         ('Sales Representative', 70000, 3);
 
-
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ('Rebecca', 'Jordan', 1, NULL),
+        ('Charlie', 'Hanover', 2, 1),
+        ('Rusty', 'Shields', 3, 1);
